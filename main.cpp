a268225cb1cfecb6d64ba76b9e32cfdc3f9033f0
zeropoint-t <tetsuyaeinstein@gmail.com>
2019-03-20 11:15:48 -0700

    <typeinfo> added

--- main.cpp
+++ main.cpp
@@ -26,10 +26,10 @@ using namespace std;
 
 int main() {
 
-    AccountMgr* am = new AccountMgr("/Users/tetsuya/Documents/Projects/CSS502AHW4/CSS502-Assignment4/data4customers.txt");
-    InventoryMgr* inv = new InventoryMgr("/Users/tetsuya/Documents/Projects/CSS502AHW4/CSS502-Assignment4/data4movies.txt");
+    AccountMgr* am = new AccountMgr("data4customers.txt");
+    InventoryMgr* inv = new InventoryMgr("data4movies.txt");
     TransactionMgr tmgr(inv, am);
-    tmgr.buildTransactions("/Users/tetsuya/Documents/Projects/CSS502AHW4/CSS502-Assignment4/data4commands.txt");
+    tmgr.buildTransactions("data4commands.txt");
 
     cout << endl;
     cout << "GREAT JOB. PROJECT DONE!!!" << endl;
